51754c2f7e970d63dd319ed418fbe2412dafdcab
Duckworth <eltaconoob7@gmail.com>
2023-05-29 17:59:39 -0600

    Update modal

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const openSide = () => {
     document.getElementById("sideBar").style.width="150px"
+
+
 }
 const closeSide = () => {
     document.getElementById("sideBar").style.width="0"
 }
+
+const openModal = (element) => {
+    document.getElementById("modal1").style.display = "block"
+    document.getElementById("imgModal").src = element.src
+    var captionText = document.getElementById("modalText")
+    captionText.innerHTML = element.alt;
+}   
