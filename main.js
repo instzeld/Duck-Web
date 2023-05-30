6e8bd50a3605882ff36a2d86a1d0084dfa0ef95f
Duckworth <eltaconoob7@gmail.com>
2023-05-29 19:32:58 -0600

    Update navbar sticky (testing)

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+window.onscroll = function() {nav()};
+
 const openSide = () => {
     document.getElementById("sideBar").style.width="150px"
 
@@ -13,3 +15,17 @@ const openModal = (element) => {
     var captionText = document.getElementById("modalText")
     captionText.innerHTML = element.alt;
 }   
+
+
+
+var navbar = document.getElementById("navL");
+
+var sticky = navbar.offsetTop;
+
+function nav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
